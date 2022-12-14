6f35d796d7af1ff2a39f915df835597e3a5bd840
julik002 <117649745+julik002@users.noreply.github.com>
2022-12-14 17:41:52 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,16 @@
+﻿var
+   filetext: text;
+   a:string;
+   i:integer;
+begin
+assign(filetext,'text.txt');
+rewrite(filetext);
+for i:=1 to 10 do
+  writeln(filetext,i);
+reset(filetext);
+for i:=1 to 10 do begin
+    readln(filetext,a);
+      writeln(a);
+end;
+close(filetext);
+end.
